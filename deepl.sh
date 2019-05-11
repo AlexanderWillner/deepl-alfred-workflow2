efb0f160a46ba13b736aafe3950386047fc519c7
Alexander Willner <alex@willner.ws>
2019-05-11 20:42:10 +0200

    cleanup.

--- deepl.sh
+++ deepl.sh
@@ -55,24 +55,22 @@ fi
 query="$(echo "$query" | sed 's/.$//')"
 query="$(echo "$query" | sed 's/\"/\\\"/g')"
 query="$(echo "$query" | sed "s/'/\\\'/g")"
-data='{"jsonrpc":"2.0","method": "LMT_handle_jobs","params":{"jobs":[{"kind":"default","raw_en_sentence":"'"$query"'","raw_en_context_before":[],"raw_en_context_after":[],"quality":"fast"}],"lang":{"user_preferred_langs":["EN","DE"],"source_lang_user_selected":"auto","target_lang":"'"${LANGUAGE:-EN}"'"},"priority":1,"timestamp":1557063997314},"id":69120002}'
+data='{"jsonrpc":"2.0","method": "LMT_handle_jobs","params":{"jobs":[{"kind":"default","raw_en_sentence":"'"$query"'","raw_en_context_before":[],"raw_en_context_after":[],"quality":"fast"}],"lang":{"user_preferred_langs":["EN","DE"],"source_lang_user_selected":"auto","target_lang":"'"${LANGUAGE:-EN}"'"},"priority":-1,"timestamp":1557063997314},"id":69120002}'
 contentlen="$(($(echo $data | wc -c) - 1))"
 HEADER=(\
+  --compressed \
+  -H 'Origin: https://www.deepl.com' \
+  -H 'Referer: https://www.deepl.com/translator' \
+  -H 'Accept: */*' \
+  -H 'Content-Type: text/plain' \
   -H 'Accept-Language: en-us' \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15' \
-  -H 'Connection: keep-alive' )
+)
 ###############################################################################
 
 # pre query ###################################################################
 curl -s 'https://www.deepl.com/PHP/backend/clientState.php?request_type=jsonrpc&il=EN' \
--XPOST \
 "${HEADER[@]}" \
--H 'Content-Type: text/plain' \
--H 'Accept: */*' \
--H 'Accept-Encoding: br, gzip, deflate' \
--H 'Host: www.deepl.com' \
--H 'Origin: https://www.deepl.com' \
--H 'Referer: https://www.deepl.com/translator' \
 -H 'Content-Length: 83' \
 -H 'X-Requested-With: XMLHttpRequest' \
 --data-binary '{"jsonrpc":"2.0","method":"getClientState","params":{"v":"20180814"},"id":69120001}' >| /dev/null
@@ -80,16 +78,8 @@ curl -s 'https://www.deepl.com/PHP/backend/clientState.php?request_type=jsonrpc&
 
 # query #######################################################################
 result=$(curl -s 'https://www2.deepl.com/jsonrpc' \
--XPOST \
 "${HEADER[@]}" \
--H 'Accept: */*' \
--H 'Accept-Encoding: br, deflate' \
--H 'Content-Type: text/plain' \
--H 'Origin: https://www.deepl.com' \
--H 'Content-Length: '$contentlen \
--H 'Host: www2.deepl.com' \
--H 'Referer: https://www.deepl.com/translator' \
---data-binary "$data")
+--data-binary $"$data")
 
 if [[ "$result" == *'"error":{"code":'* ]] ; then
   message=$(echo "$result"|"${PARSER}" -r '.["error"]|.message')

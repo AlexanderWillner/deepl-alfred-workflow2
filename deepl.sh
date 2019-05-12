3cc0df967dc208b184946f9d1f310a11439d74b4
Alexander Willner <alex@willner.ws>
2019-05-12 19:17:30 +0200

    styling

--- deepl.sh
+++ deepl.sh
@@ -4,13 +4,13 @@
 #set -o errexit -o pipefail -o noclobber -o nounset
 PATH="$PATH:/usr/local/bin/"
 PARSER="jq"
-if ! type "${PARSER}" >/dev/null 2>&1; then
+if ! type "$PARSER" >/dev/null 2>&1; then
   PARSER="./jq-dist"
 fi
 ###############################################################################
 
 # helper functions ############################################################
-function printJson {
+function printJson() {
   echo '{"items": [{"uid": null,"arg": "'"$1"'","valid": "yes","autocomplete": "autocomplete","title": "'"$1"'"}]}'
 }
 ###############################################################################
@@ -57,34 +57,34 @@ query="$(echo "$query" | sed 's/\"/\\\"/g')"
 query="$(echo "$query" | sed "s/'/\\\'/g")"
 data='{"jsonrpc":"2.0","method": "LMT_handle_jobs","params":{"jobs":[{"kind":"default","raw_en_sentence":"'"$query"'","raw_en_context_before":[],"raw_en_context_after":[],"quality":"fast"}],"lang":{"user_preferred_langs":["EN","DE"],"source_lang_user_selected":"auto","target_lang":"'"${LANGUAGE:-EN}"'"},"priority":-1,"timestamp":1557063997314},"id":69120002}'
 contentlen="$(($(echo $data | wc -c) - 1))"
-HEADER=(\
-  --compressed \
-  -H 'Origin: https://www.deepl.com' \
-  -H 'Referer: https://www.deepl.com/translator' \
-  -H 'Accept: */*' \
-  -H 'Content-Type: text/plain' \
-  -H 'Accept-Language: en-us' \
-  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15' \
+HEADER=(
+  --compressed
+  -H 'Origin: https://www.deepl.com'
+  -H 'Referer: https://www.deepl.com/translator'
+  -H 'Accept: */*'
+  -H 'Content-Type: text/plain'
+  -H 'Accept-Language: en-us'
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15'
 )
 ###############################################################################
 
 # pre query ###################################################################
 curl -s 'https://www.deepl.com/PHP/backend/clientState.php?request_type=jsonrpc&il=EN' \
-"${HEADER[@]}" \
--H 'Content-Length: 83' \
--H 'X-Requested-With: XMLHttpRequest' \
---data-binary '{"jsonrpc":"2.0","method":"getClientState","params":{"v":"20180814"},"id":69120001}' >| /dev/null
+  "${HEADER[@]}" \
+  -H 'Content-Length: 83' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  --data-binary '{"jsonrpc":"2.0","method":"getClientState","params":{"v":"20180814"},"id":69120001}' >|/dev/null
 ###############################################################################
 
 # query #######################################################################
 result=$(curl -s 'https://www2.deepl.com/jsonrpc' \
-"${HEADER[@]}" \
---data-binary $"$data")
+  "${HEADER[@]}" \
+  --data-binary $"$data")
 
-if [[ "$result" == *'"error":{"code":'* ]] ; then
-  message=$(echo "$result"|"${PARSER}" -r '.["error"]|.message')
+if [[ $result == *'"error":{"code":'* ]]; then
+  message=$(echo "$result" | "$PARSER" -r '.["error"]|.message')
   printJson "Error: $message"
 else
-  echo $result|"${PARSER}" -r '{items: [.result.translations[0].beams[] | {uid: null, arg:.postprocessed_sentence, valid: "yes", autocomplete: "autocomplete",title: .postprocessed_sentence}]}'
+  echo "$result" | "$PARSER" -r '{items: [.result.translations[0].beams[] | {uid: null, arg:.postprocessed_sentence, valid: "yes", autocomplete: "autocomplete",title: .postprocessed_sentence}]}'
 fi
 ###############################################################################
